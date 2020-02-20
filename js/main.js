cff73bf025fc819c13e51ab7353d0032d7b48d6a
kanda <rodrigokanda2016@gmail.com>
2020-02-20 15:39:25 -0500

    still working...

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log('Your document is ready!');
-    var elem = document.querySelector('.carousel');
-    var flkty = new Flickity(elem, {
-        // options
-        cellAlign: 'left',
-        contain: true
-    });
+  console.log('Your document is ready!');
+  var elem = document.querySelector('.carousel');
+  var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
 
-    // element argument can be a selector string
-    //   for an individual element
-    var flkty = new Flickity('.carousel', {
-        // options
-    });
+  // element argument can be a selector string
+  //   for an individual element
+  var flkty = new Flickity('.carousel', {
+    // options
+  });
 });
+window.onscroll = function () { myFunction() };
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("fixed-header");
+  } else {
+    header.classList.remove("fixed-header");
+  }
+}
