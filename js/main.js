c62832cd395174da9e6f2fa2595a656521bc6192
kanda <rodrigokanda2016@gmail.com>
2020-03-02 14:52:31 -0500

    chore:

--- js/main.js
+++ js/main.js
@@ -13,15 +13,42 @@ document.addEventListener("DOMContentLoaded", function () {
     // options
   });
 });
-window.onscroll = function () { myFunction() };
+// window.onscroll = function () { myFunction() };
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
+// var header = document.getElementById("myHeader");
+// var sticky = header.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("fixed-header");
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("fixed-header");
+
+
+//   } else {
+//     header.classList.remove("fixed-header");
+
+
+//   }
+// }
+//validate form
+let inputEmail = document.getElementById('input-email')
+inputEmail.addEventListener('input', (e) => {
+
+  if (inputEmail.validity.typeMismatch) {
+    inputEmail.setCustomValidity("I am expecting an e-mail address")
   } else {
-    header.classList.remove("fixed-header");
+    inputEmail.setCustomValidity("Please enter an e-mail valid");
   }
-}
+
+
+})
+var clicks = 0;
+function onClick() {
+  clicks += 1;
+
+  document.getElementById("clicks").innerHTML = clicks;
+  document.getElementById("clicks").style.background = "red"
+  document.getElementById("clicks").style.width = "40px"
+  document.getElementById("clicks").style.height = "40px"
+  document.getElementById("clicks").style.borderRadius = "50px"
+
+};
